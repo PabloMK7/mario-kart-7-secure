5a6be3e2a6c5311a61fe74108e91fb3e84027d91
Daniel López Guimaraes <danielectra@outlook.com>
2024-03-17 22:38:25 +0000

    Update with latest changes

--- nex/register_common_authentication_server_protocols.go
+++ nex/register_common_authentication_server_protocols.go
@@ -2,9 +2,11 @@ package nex
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/PretendoNetwork/mario-kart-7/globals"
 	nex_ticket_granting "github.com/PretendoNetwork/mario-kart-7/nex/ticket-granting"
+	"github.com/PretendoNetwork/nex-go/constants"
 	"github.com/PretendoNetwork/nex-go/types"
 	common_ticket_granting "github.com/PretendoNetwork/nex-protocols-common-go/ticket-granting"
 	ticket_granting "github.com/PretendoNetwork/nex-protocols-go/ticket-granting"
@@ -17,15 +19,17 @@ func registerCommonAuthenticationServerProtocols() {
 	ticketGrantingProtocol.SetHandlerLoginEx(nex_ticket_granting.LoginEx)
 	ticketGrantingProtocol.SetHandlerRequestTicket(nex_ticket_granting.RequestTicket)
 
+	port, _ := strconv.Atoi(os.Getenv("PN_MK7_SECURE_SERVER_PORT"))
+
 	secureStationURL := types.NewStationURL("")
-	secureStationURL.Scheme = "prudps"
-	secureStationURL.Fields["address"] = os.Getenv("PN_MK7_SECURE_SERVER_HOST")
-	secureStationURL.Fields["port"] = os.Getenv("PN_MK7_SECURE_SERVER_PORT")
-	secureStationURL.Fields["CID"] = "1"
-	secureStationURL.Fields["PID"] = "2"
-	secureStationURL.Fields["sid"] = "1"
-	secureStationURL.Fields["stream"] = "10"
-	secureStationURL.Fields["type"] = "2"
+	secureStationURL.SetURLType(constants.StationURLPRUDPS)
+	secureStationURL.SetAddress(os.Getenv("PN_MK7_SECURE_SERVER_HOST"))
+	secureStationURL.SetPortNumber(uint16(port))
+	secureStationURL.SetConnectionID(1)
+	secureStationURL.SetPrincipalID(types.NewPID(2))
+	secureStationURL.SetStreamID(1)
+	secureStationURL.SetStreamType(constants.StreamTypeRVSecure)
+	secureStationURL.SetType(uint8(constants.StationURLFlagPublic))
 
 	globals.CommonTicketGrantingProtocol.SecureStationURL = secureStationURL
 	globals.CommonTicketGrantingProtocol.BuildName = types.NewString(serverBuildString)
